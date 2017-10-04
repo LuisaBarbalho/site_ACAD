c4442b5f62183989e361ea72567657d861a28c08
LuisaBarbalho <luisabps95@gmail.com>
2017-10-04 12:35:13 -0300

    adiciona site inicial

---
+++ view/inscricao_linux.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ACAD | linux</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <!-- barra de navegação -->
+  <nav id="custom-bootstrap-menu" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+
+      <!-- Logo -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand">ACAD</a>
+      </div>
+
+      <!-- itens do menu -->
+      <div class="collapse navbar-collapse" id="mainNavbar">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="./home.php">Home</a></li>
+          <li><a href="./sobre.php">Sobre</a></li>
+          <li><a href="./cursos.php">Cursos</a></li>
+          <li class="active"><a href="./inscreva.php">Inscreva-se</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- formulario de inscrição para o curso de Linux -->
+  <div class="form">
+    <div class="container-fluid">
+      <div id="inscricao-linux" class="categoria-banner">
+        <img class="imagem-inscricao" src="../img/linux_logo.png"/>
+        <div class="categoria-titulo">
+          Linux
+        </div>
+      </div>
+
+
+      <form action="../controller/controller_linux.php" method="POST">
+        <div class="form-conteudo">
+          Nome:
+          <input type="text" name="_nomeCadastro"/><br/>
+
+          Email:
+          <input type="email" name="_senhaCadastro" /><br/>
+
+          Matrícula:
+          <input type="text" name="_senhaConfirmada" /><br/>
+
+          Telefone:
+          <input type="text" name="_senhaConfirmada" /><br/>
+
+          Data de Nascimento:
+          <input type="text" name="_senhaConfirmada" /><br/>
+
+          <div>
+            <input type="submit" value="Enviar">
+          </div>
+
+        </div>
+      </form>
+    </div>
+  </div>
+
+</body>
+</html>
---
+++ view/sobre.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>ACAD | sobre</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+  <!-- barra de navegação -->
+  <nav id="custom-bootstrap-menu" class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+
+      <!-- Logo -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand">ACAD</a>
+      </div>
+
+      <!-- itens do menu -->
+      <div class="collapse navbar-collapse" id="mainNavbar">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="./home.php">Home</a></li>
+          <li class="active"><a href="./sobre.php">Sobre</a></li>
+          <li><a href="./cursos.php">Cursos</a></li>
+          <li><a href="./inscreva.php">Inscreva-se</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+    <!-- banner inicial contendo o titulo da categoria -->
+  <div class="categoria-banner">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="categoria-titulo">
+            Sobre
+          </div>
+        </div>
+
+          <!-- icone utilizado no banner -->
+        <div class="col-md-6">
+          <div class="categoria-icones">
+            <i class="material-icons" style="font-size:150px">insert_drive_file</i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
